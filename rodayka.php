3357d6f4a5f4200ca4b7622ab703eae373ae9d1e
Ingo Stramm <ingostramm@gmail.com>
2020-09-23 11:16:33 -0300

    implementando o auto-updater

--- rodayka.php
+++ rodayka.php
@@ -1,4 +1,5 @@
-<?php  
+<?php
+
 /**
  * Plugin Name: Rodayka
  * Plugin URI: https://agencialaf.com
@@ -9,13 +10,20 @@
  * License: GPLv2
  */
 
-defined( 'ABSPATH' )or die( 'No script kiddies please!' );
+defined('ABSPATH') or die('No script kiddies please!');
 
-define( 'RK_DIR', plugin_dir_path( __FILE__ ) );
-define( 'RK_URL', plugin_dir_url( __FILE__ ) );
+define('RK_DIR', plugin_dir_path(__FILE__));
+define('RK_URL', plugin_dir_url(__FILE__));
 
 require_once RK_DIR . 'tgm/tgm.php';
 require_once RK_DIR . 'function.php';
 require_once RK_DIR . 'cmb.php';
 require_once RK_DIR . 'shortcode.php';
 require_once RK_DIR . 'scripts.php';
+
+require 'plugin-update-checker-4.10/plugin-update-checker.php';
+$updateChecker = Puc_v4_Factory::buildUpdateChecker(
+    'https://raw.githubusercontent.com/IngoStramm/rodayka/master/info.json',
+    __FILE__,
+    'rodayka'
+);
